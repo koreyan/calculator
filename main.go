793b938e6e08ef0b8b51ce8b2e9b0c61dd646e64
yoan <plfor30y@naver.com>
2022-05-07 11:48:44 +0900

    2022 05 07 계산기 기능 완벽하게 구현

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"calculator/calc"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Print("계산기 수식을 적어주세요\n")
+	reader := bufio.NewReader(os.Stdin)
+	exp, err := reader.ReadString('\n')
+	exp = strings.TrimSpace(exp) // 문자열의 쓸모없는 양쪽 공백을 없앰
+	fmt.Println(exp)
+	if err != nil {
+		panic("수식이 올바르지 않습니다.")
+	}
+
+	postfix := calc.ToPostfix(exp)
+	fmt.Printf("결과 : %d", calc.Calculate(postfix))
+
+}
